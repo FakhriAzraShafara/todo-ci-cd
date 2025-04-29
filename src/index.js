0ff88db2077189961cb2b9e51f3772dd301d5483
mfisya <fakhrozan.pp@gmail.com>
2025-04-29 11:28:59 +0700

    create todo

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
